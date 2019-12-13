9498e82dd3f94524d5ccbb37973b17d3d0a122c3
Chang Hyun Ji <nigostarrgit.github.com>
2019-12-13 12:59:43 +0900

    #1.6 React navigation's done
    Make main and tab navigation.

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -2,16 +2,18 @@ import React from "react";
 import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createBottomTabNavigator } from "react-navigation-tabs";
+import { createStackNavigator } from "react-navigation-stack";
 import MoviesScreen from "../screens/Movies";
 import TVScreen from "../screens/TV";
 import SearchScreen from "../screens/Search";
 import { BG_COLOR } from "../constants/Colors";
 import TabBarIcon from "../components/TabBarIcon";
+import { createStack } from "./config";
 
 const TabNavigation = createBottomTabNavigator(
   {
     Movie: {
-      screen: MoviesScreen,
+      screen: createStack(MoviesScreen, "Movies"),
       navigationOptions: {
         tabBarIcon: ({ focused }) => (
           <TabBarIcon
@@ -21,8 +23,9 @@ const TabNavigation = createBottomTabNavigator(
         )
       }
     },
+
     TV: {
-      screen: TVScreen,
+      screen: createStack(TVScreen, "TV"),
       navigationOptions: {
         tabBarIcon: ({ focused }) => (
           <TabBarIcon
@@ -32,8 +35,9 @@ const TabNavigation = createBottomTabNavigator(
         )
       }
     },
+
     Search: {
-      screen: SearchScreen,
+      screen: createStack(SearchScreen, "Search"),
       navigationOptions: {
         tabBarIcon: ({ focused }) => (
           <TabBarIcon
