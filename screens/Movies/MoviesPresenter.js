b70ef330871987314ff1b6d89c752b8495c1535f
Chang Hyun Ji <nigostarrgit.github.com>
2019-12-16 15:23:36 +0900

    #1.8 Start to make Slider

---
+++ screens/Movies/MoviesPresenter.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Loader from "../../components/Loader";
+import styled from "styled-components";
+import MovieSlider from "../../components/MovieSlider";
+import { BG_COLOR } from "../../constants/Colors";
+
+const Container = styled.ScrollView`
+  background-color: ${BG_COLOR};
+`;
+
+const MoviesPresenter = ({ loading, upcoming, popular, nowPlaying }) =>
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      <MovieSlider movies={nowPlaying} />
+    </Container>
+  );
+
+MoviesPresenter.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  upcoming: PropTypes.array,
+  popular: PropTypes.array,
+  nowPlaying: PropTypes.array
+};
+
+export default MoviesPresenter;
